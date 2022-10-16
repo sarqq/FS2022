978896d3deaa4cc104c5800979e247e480e55323
sarqq <baba.sara@hotmail.com>
2022-10-16 03:33:26 +0300

    Exercises 1.3. and 1.4.

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,53 +1,50 @@
 import './App.js';
 
 const Header = (props) => {
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
-  return(
-    <>
-      <p>{props.part} {props.exercises}</p>
-    </>
-  )
+  return<p>{props.part} {props.exercises}</p>
 }
 
 const Content = (props) => {
   return(
     <div>
-      <Part part={props.param1} exercises={props.param2}/>
-      <Part part={props.param3} exercises={props.param4}/>
-      <Part part={props.param5} exercises={props.param6}/>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
   )
 }
 
 const Total = (props) => {
-  return(
-    <div>
-      <p>Number of exercises {props.a}+{props.b}+{props.c}</p>
-    </div>
-  )
+  return<p>Number of exercises {props.parts[0].exercises}+{props.parts[1].exercises}+{props.parts[2].exercises}</p>
 }
 
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3  = 14
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course}/>
-      <Content param1={part1} param2={exercises1} param3={part2} param4={exercises2} param5={part3} param6={exercises3}/>
-      <Total a={exercises1} b={exercises2} c={exercises3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </>
   );
 }
