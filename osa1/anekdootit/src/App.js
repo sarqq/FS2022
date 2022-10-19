46246b47dc2ae0f3abb187f9e9357f1a4dd91e78
sarqq <baba.sara@hotmail.com>
2022-10-19 14:34:33 +0300

    Exercise 1.14.

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -14,20 +14,35 @@ const App = () => {
 
   const [points, setPoints] = useState([0,0,0,0,0,0,0])
   const [selected, setSelected] = useState(0)
+  const [best, setBest] = useState(0)
   
+  const findBest = () => {
+    let temp = 0
+    for(let i = 0; i<points.length; i++){
+      if(points[i]>=temp){
+        temp = points[i]
+        setBest(i)
+      }
+    }
+  }
+
   const vote = (index) => {
     const copy = [...points]
     let temp = copy[index]+1
     copy[index] = temp
     setPoints(copy)
+    findBest()
   }
 
   return (
     <div>
+      <h3>Anecdote of the day</h3>
       {anecdotes[selected]}<br/>
       Has {points[selected]} votes<br/>
       <button onClick={() => {vote(selected)}}>Vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))}>Next anecdote</button>
+      <h3>Anecdote with the most votes</h3>
+      {anecdotes[best]}
     </div>
   );
 }
